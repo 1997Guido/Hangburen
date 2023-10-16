e4b446fd3dc69dc6d9f885d0b3d17e4daac225d7
larson cop <97100495@st.deltion.nl>
2023-10-16 19:27:16 +0200

    Update get_word.php

--- public/get_word.php
+++ public/get_word.php
@@ -3,13 +3,12 @@
 $all_words = array(
     "dutchword1" => "germanword1",
     "dutchword2" => "germanword2",
-)
-$random_dutch_word = array_rand($words);
-$german_word = $words[$random_dutch_word];
+);
 
-$words = [$random_dutch_word, $german_word]
+$random_dutch_word = array_rand($all_words);
+$german_word = $all_words[$random_dutch_word];
 
-
+$words = [$random_dutch_word, $german_word];
 
 echo json_encode(array("words" => $words));
 ?>
