dcb8f02d922811603ebe0df43dc0665076c7eb38
larson cop <97100495@st.deltion.nl>
2023-10-16 19:50:24 +0200

    Create process.php

---
+++ public/process.php
@@ -0,0 +1,28 @@
+<?php
+// Database connection
+$servername = "your_database_server";
+$username = "your_database_username";
+$password = "your_database_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve Dutch and German words from the form
+$dutch_word = $_POST["dutch_word"];
+$german_word = $_POST["german_word"];
+
+// Insert into the database
+$sql = "INSERT INTO word_translations (dutch_word, german_word) VALUES ('$dutch_word', '$german_word')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Words submitted successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
