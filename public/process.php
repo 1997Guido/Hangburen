ee5b6268519479870805e088a1bb006d45082a31
larson cop <97100495@st.deltion.nl>
2023-10-16 20:26:34 +0200

    Update process.php

--- public/process.php
+++ public/process.php
@@ -1,28 +1,34 @@
 <?php
-// Database connection
+// Database connection using PDO
 $servername = "mariadb";
-$username = "root";
-$password = "";
+$username = "Hangman";
+$password = "a3b6c9";
 $dbname = "woord";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // Retrieve Dutch and German words from the form
+    $dutch_word = $_POST["dutch_word"];
+    $german_word = $_POST["german_word"];
 
-// Retrieve Dutch and German words from the form
-$dutch_word = $_POST["dutch_word"];
-$german_word = $_POST["german_word"];
+    // Prepare the SQL statement
+    $sql = "INSERT INTO word_translations (dutch_word, german_word) VALUES (:dutch_word, :german_word)";
+    $stmt = $conn->prepare($sql);
 
-// Insert into the database
-$sql = "INSERT INTO word_translations (dutch_word, german_word) VALUES ('$dutch_word', '$german_word')";
+    // Bind parameters
+    $stmt->bindParam(':dutch_word', $dutch_word);
+    $stmt->bindParam(':german_word', $german_word);
 
-if ($conn->query($sql) === TRUE) {
+    // Execute the statement
+    $stmt->execute();
+
     echo "Words submitted successfully!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
 
-$conn->close();
+// Close the database connection
+$conn = null;
 ?>
