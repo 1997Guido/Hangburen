450de9b79aa0a58821c3dc76428adce326bb26e2
Martijn <mhjnieuwenburg@gmail.com>
2023-10-16 16:04:14 +0200

    card and login screen
    fixed everything

--- public/index.php
+++ public/index.php
@@ -1,22 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-  <title>Hangman Game</title>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Hangman Game</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
+
 <body>
-  <h1>Hangman Game</h1>
-  <div id="wordToGuess"></div>
-  <div id="keyboard"></div>
-  <button onclick="newGame()">New Game</button>
-  <div id="hangman">
-  <div id="head" class="hide"></div>
-  <div id="body" class="hide"></div>
-  <div id="leftArm" class="hide"></div>
-  <div id="rightArm" class="hide"></div>
-  <div id="leftLeg" class="hide"></div>
-  <div id="rightLeg" class="hide"></div>
-</div>
-  <script src="js/hangman/game.js"></script>
+    <div class="screen-overlay">
+        <div class="game-box">
+            <h1>Hangman Game</h1>
+            <div id="wordToGuess"></div>
+            <div id="hangman">
+                <div id="head" class="hide"></div>
+                <div id="body" class="hide"></div>
+                <div id="leftArm" class="hide"></div>
+                <div id="rightArm" class="hide"></div>
+                <div id="leftLeg" class="hide"></div>
+                <div id="rightLeg" class="hide"></div>
+            </div>
+            <div id="keyboard"></div>
+            <button onclick="newGame()">New Game</button>
+        </div>
+
+    </div>
+
+
 </body>
+<script src="js/hangman/game.js"></script>
+
 </html>
