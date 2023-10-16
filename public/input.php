5d9b1dfd313c121642ce8d5384ad88ee661a23eb
larson cop <97100495@st.deltion.nl>
2023-10-16 15:36:52 +0200

    Create input.php

---
+++ public/input.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Word Submission</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        <label for="Dutch-Word">Enter a Word:</label>
+        <input type="text" class="word" name="word" id="word" required>
+        <label for="German-Word">Enter a Word:</label>
+        <input type="text" class="word" name="word" id="word" required>
+        <button type="submit" class="ibutton">Submit</button>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+
