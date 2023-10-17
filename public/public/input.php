255eba4104a0f8149cf5d9623fe34d8ac55a8fa9
larson cop <97100495@st.deltion.nl>
2023-10-17 10:47:12 +0200

    update

--- public/public/input.php
+++ public/public/input.php
@@ -10,6 +10,14 @@
         <br>
         <label for="german-word">Enter a German Word:</label>
         <input type="text" class="word" name="german_word" id="german-word" required>
+        <br>
+        <label for="difficulty">Select Difficulty:</label>
+        <select name="difficulty" id="difficulty">
+            <option value="easy">Easy</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="expert">Expert</option>
+        </select>
+        <br>
         <button type="submit" class="ibutton">Submit</button>
     </form>
 </body>
