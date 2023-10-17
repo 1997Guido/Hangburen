cd02469180a82e59b46c9adbf07b2fb3155c9b0c
Martijn <mhjnieuwenburg@gmail.com>
2023-10-17 09:12:08 +0200

    udpate

--- public/index.php
+++ public/index.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Hangman Game</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Hangman Game</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
-  <h1>Hangman Game</h1>
-  <div id="wordToGuess"></div>
-  <div id="keyboard"></div>
-  <button onclick="newGame()">New Game</button>
-  <script src="script.js"></script>
+    <div class="screen-overlay">
+        <div class="returnHome"><a href="./login.php">Home</a>
+        </div>
+        <div class="game-box">
+            <h1>Hangman Game</h1>
+            <div id="wordToGuess"></div>
+            <div id="hangman">
+                <div id="head" class="hide"></div>
+                <div id="body" class="hide"></div>
+                <div id="leftArm" class="hide"></div>
+                <div id="rightArm" class="hide"></div>
+                <div id="leftLeg" class="hide"></div>
+                <div id="rightLeg" class="hide"></div>
+            </div>
+            <div id="keyboard"></div>
+            <button onclick="newGame()">New Game</button>
+        </div>
+
+    </div>
+
+
 </body>
+
 </html>
--- public/js/hangman/game.js
+++ public/js/hangman/game.js
@@ -1,78 +1,76 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    newGame();
-  });
-  
-  let word = "";
-  let maskedWord = "";
-  let attempts = 0;
-  
-  function newGame() {
-    fetch('get_word.php')
-    .then(response => response.json())
-    .then(data => {
+document.addEventListener("DOMContentLoaded", (event) => {
+  newGame();
+});
+
+let word = "";
+let maskedWord = "";
+let attempts = 0;
+
+function newGame() {
+  fetch("get_word.php")
+    .then((response) => response.json())
+    .then((data) => {
       word = data.word.toUpperCase();
       maskedWord = "_".repeat(word.length);
       document.getElementById("wordToGuess").textContent = maskedWord;
       generateKeyboard();
       attempts = 0;
     });
-  }
+}
 
-  function saveHighScore(username, score) {
-    fetch('save_score.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `username=${username}&score=${score}`
-    })
-    .then(response => response.json())
-    .then(data => {
+function saveHighScore(username, score) {
+  fetch("save_score.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `username=${username}&score=${score}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
       if (data.status === "success") {
         alert("High score saved!");
       }
     });
+}
+if (!maskedWord.includes("_")) {
+  alert("You won!");
+  let username = prompt("Enter your username to save your high score:");
+  let score = calculateScore(); // Implement your own score calculation logic
+  saveHighScore(username, score);
+  newGame();
+}
+
+function generateKeyboard() {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let keyboardHtml = "";
+
+  for (let letter of alphabet) {
+    keyboardHtml += `<button onclick="makeGuess('${letter}')">${letter}</button>`;
   }
-  if (!maskedWord.includes("_")) {
-    alert("You won!");
-    let username = prompt("Enter your username to save your high score:");
-    let score = calculateScore(); // Implement your own score calculation logic
-    saveHighScore(username, score);
-    newGame();
-  }
-  
-  
-  function generateKeyboard() {
-    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let keyboardHtml = "";
-  
-    for (let letter of alphabet) {
-      keyboardHtml += `<button onclick="makeGuess('${letter}')">${letter}</button>`;
-    }
-  
-    document.getElementById("keyboard").innerHTML = keyboardHtml;
-  }
-  
-  function makeGuess(letter) {
-    let newMasked = "";
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === letter) {
-        newMasked += letter;
-      } else {
-        newMasked += maskedWord[i];
-      }
-    }
-  
-    if (newMasked === maskedWord) {
-      attempts++;
-      // Handle failed attempts
+
+  document.getElementById("keyboard").innerHTML = keyboardHtml;
+}
+
+function makeGuess(letter) {
+  let newMasked = "";
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      newMasked += letter;
     } else {
-      maskedWord = newMasked;
-      document.getElementById("wordToGuess").textContent = maskedWord;
-      if (!maskedWord.includes("_")) {
-        alert("You won!");
-        newGame();
-      }
+      newMasked += maskedWord[i];
     }
   }
-  
+
+  if (newMasked === maskedWord) {
+    attempts++;
+    // Handle failed attempts
+  } else {
+    maskedWord = newMasked;
+    document.getElementById("wordToGuess").textContent = maskedWord;
+    if (!maskedWord.includes("_")) {
+      alert("You won!");
+      newGame();
+    }
+  }
+}
