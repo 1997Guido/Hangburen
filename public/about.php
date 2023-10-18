d0e16d2d0977fa2ce6c1ec34e01238c56ba9273f
larson cop <97100495@st.deltion.nl>
2023-10-18 10:50:27 +0200

    Commit

--- public/about.php
+++ public/about.php
@@ -10,12 +10,20 @@
 </head>
 
 <body>
-<div class="screen-overlay">
-    
-
-
+<div class="container">
+        <h2>Welcome to Hangburen</h2>
+        <p>"Hangburen" is a game inspired by Hangman. In "Hangburen," players can compete against and/or with each other. Players are presented with a Dutch or German word in turns, which they must guess and translate. With each incorrect guess, a gallows and a figure hanging from it is progressively drawn. After 10 turns, you lose. Each player must guess 3 words, and afterwards, a score is calculated based on the time taken per word and the average number of mistakes made per word.</p>
+        <p>The name "Hangburen" is derived from the word "Hang," from Hangman, and "Buren," the Dutch word for neighbors.</p>
+        <h3>Nederlands:</h3>
+        <p>"Hangburen" is een spel geïnspireerd door Galgje. In "Hangburen" kunnen spelers tegen en/of met elkaar spelen. Spelers krijgen om de beurt een Nederlands of Duits woord gepresenteerd, dat ze moeten raden en vertalen. Bij elke foutieve gok wordt er een galg en een figuur aan de galg getekend. Na 10 beurten ben je af. Elke speler moet 3 woorden raden, en daarna wordt een score berekend op basis van de tijd die per woord is genomen en het gemiddelde aantal gemaakte fouten per woord.</p>
+        <p>De naam "Hangburen" komt van het woord "Hang", van Hangman (galgje), en "Buren", het Nederlandse woord voor buren.</p>
+        <h3>Duits:</h3>
+        <p>"Hangburen" ist ein Spiel, das von Hangman inspiriert ist. In "Hangburen" können Spieler gegeneinander und/oder miteinander antreten. Den Spielern werden abwechselnd niederländische oder deutsche Wörter präsentiert, die sie erraten und übersetzen müssen. Mit jedem falschen Tipp wird eine Galgenkonstruktion und eine daran hängende Figur schrittweise gezeichnet. Nach 10 Runden verliert man. Jeder Spieler muss 3 Wörter erraten, und danach wird eine Punktzahl basierend auf der benötigten Zeit pro Wort und der durchschnittlichen Anzahl von Fehlern pro Wort berechnet.</p>
+        <p>Der Name "Hangburen" leitet sich von dem Wort "Hang" aus Hangman und "Buren" ab, dem niederländischen Wort für Nachbarn.</p>
+    </div>
 
 </div>
+
 
 
 </body>
