4a73735a19dc7383bc9901c04bd9e4edf0ad9000
larson cop <97100495@st.deltion.nl>
2023-10-18 10:27:06 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Hangman Game</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" href="./images/logo-trans.png">
+</head>
+
+<body>
+<div>
+      
+
+</div>
+
+
+</body>
+
+
+</html>
