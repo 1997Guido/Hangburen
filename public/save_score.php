358b1249c3eae8f644c6c3a8c4c456cf30bc9eec
Guido Erdtsieck <guidoerdtsieck@hotmail.nl>
2023-10-18 10:52:05 +0200

    saving scores

--- public/save_score.php
+++ public/save_score.php
@@ -9,84 +9,95 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Check if the database exists
-    $dbname = "your_database_name";
     $checkDbStmt = $conn->prepare("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = :dbname");
     $checkDbStmt->bindParam(':dbname', $dbname);
     $checkDbStmt->execute();
 
     if ($checkDbStmt->rowCount() == 0) {
-        // Create the database if it doesn't exist
         $createDbStmt = $conn->prepare("CREATE DATABASE $dbname");
         $createDbStmt->execute();
         echo "Database created successfully.";
     } else {
         echo "Database already exists. Nothing to do.";
-
-        // Connect to the existing database
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
+    // Create the game_scores table if it doesn't exist
+    $createTableStmt = $conn->prepare("
+    CREATE TABLE IF NOT EXISTS game_scores (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        score INT NOT NULL
+    )
+    ");
+    $createTableStmt->execute();
+
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 
+function handleScore($conn, $username, $score) {
+    $stmt = $conn->prepare("SELECT score FROM game_scores WHERE username = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        $currentScore = $row['score'];
+
+        if ($score > $currentScore) {
+            $updateStmt = $conn->prepare("UPDATE game_scores SET score = :score WHERE username = :username");
+            $updateStmt->bindParam(':score', $score);
+            $updateStmt->bindParam(':username', $username);
+            if ($updateStmt->execute()) {
+                echo "Score updated successfully for $username!";
+            } else {
+                echo "Error updating score for $username: " . implode(" ", $updateStmt->errorInfo());
+            }
+        } else {
+            echo "No update needed for $username. Existing score is higher.";
+        }
+    } else {
+        $insertStmt = $conn->prepare("INSERT INTO game_scores (username, score) VALUES (:username, :score)");
+        $insertStmt->bindParam(':username', $username);
+        $insertStmt->bindParam(':score', $score);
+        if ($insertStmt->execute()) {
+            echo "Score added successfully for $username!";
+        } else {
+            echo "Error adding score for $username: " . implode(" ", $insertStmt->errorInfo());
+        }
+    }
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if data is sent as JSON
     $input = json_decode(file_get_contents('php://input'), true);
 
     if ($input) {
-        $username = $input["username"];
-        $highscore = $input["highscore"];
+        $germanPlayername = $input["germanPlayername"];
+        $germanPlayerScore = $input["germanPlayerScore"];
+        $dutchPlayername = $input["dutchPlayername"];
+        $dutchPlayerScore = $input["dutchPlayerScore"];
 
-        $stmt = $conn->prepare("SELECT highscore FROM game_scores WHERE username = :username");
-        $stmt->bindParam(':username', $username);
-        $stmt->execute();
-
-        if ($stmt->rowCount() > 0) {
-            $row = $stmt->fetch();
-            $currentHighscore = $row['highscore'];
-
-            if ($highscore > $currentHighscore) {
-                $updateStmt = $conn->prepare("UPDATE game_scores SET highscore = :highscore WHERE username = :username");
-                $updateStmt->bindParam(':highscore', $highscore);
-                $updateStmt->bindParam(':username', $username);
-                if ($updateStmt->execute()) {
-                    echo "Highscore updated successfully!";
-                } else {
-                    echo "Error updating highscore: " . implode(" ", $updateStmt->errorInfo());
-                }
-            } else {
-                echo "No update needed. Existing highscore is higher.";
-            }
-        } else {
-            $insertStmt = $conn->prepare("INSERT INTO game_scores (username, highscore) VALUES (:username, :highscore)");
-            $insertStmt->bindParam(':username', $username);
-            $insertStmt->bindParam(':highscore', $highscore);
-            if ($insertStmt->execute()) {
-                echo "Highscore added successfully!";
-            } else {
-                echo "Error adding highscore: " . implode(" ", $insertStmt->errorInfo());
-            }
-        }
+        handleScore($conn, $germanPlayername, $germanPlayerScore);
+        handleScore($conn, $dutchPlayername, $dutchPlayerScore);
     } else {
         echo "Invalid JSON data received.";
     }
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $stmt = $conn->prepare("SELECT username, highscore FROM game_scores ORDER BY highscore DESC");
+    $stmt = $conn->prepare("SELECT username, score FROM game_scores ORDER BY score DESC");
     $stmt->execute();
 
     if ($stmt->rowCount() > 0) {
-        echo "Highscores:<br>";
+        echo "Scores:<br>";
         while ($row = $stmt->fetch()) {
-            echo $row['username'] . ": " . $row['highscore'] . "<br>";
+            echo $row['username'] . ": " . $row['score'] . "<br>";
         }
     } else {
-        echo "No highscores found in the database.";
+        echo "No scores found in the database.";
     }
 }
 
-// Close the connection
 $conn = null;
 ?>
